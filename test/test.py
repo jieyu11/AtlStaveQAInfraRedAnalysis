aa865e783feb4b73c33a082ed137decfaec6433a
Lubos Vozdecky <lubos.vozdecky@cern.ch>
2020-04-03 17:53:38 +0100

    reorganizing the folders

--- test/test.py
+++ test/test.py
@@ -1,36 +1,18 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 31 15:18:14 2020
+#!/usr/bin/env python
+import configparser
 
-@author: vozdecky
-"""
+config = configparser.ConfigParser()
 
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-import csv
+config.read("parameters.cfg")
 
-image = []
-with open('CSV_with_0.92_emmy.csv') as csvfile:
-  reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-  for row in reader:
-    image.append(row)
+Tin = config["Default"]["Tin"]
+Tout = config["Default"]["Tout"]
+Cliq = config["Default"]["Cliq"]
+Zcut = config["Default"]["Zcut"]
+FR = config["Default"]["FR"]
 
-img = np.array(image)
+#print(config.items("Default"))
 
-height = img.shape[0]
-width = img.shape[1]
+for variable in config.items("Default"):
+  print(variable[0] + " = " + variable[1]) 
 
-upper_img = img[0:int(height/2), 0:width].transpose()
-lower_img = img[int(height/2):height, 0:width].transpose()
-
-
-
-test = np.ones((600,400))*255
-
-img2 = (img - np.min(img))/(np.max(img)-np.min(img))*200 + 50 
-cv2.rectangle(img2,(100,100),(200,200),(0,0,0),thickness=3)
-
-plt.imshow(img2)
-plt.show()
