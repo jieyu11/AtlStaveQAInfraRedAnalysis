f36a917d4b7f98c759230b4e0d4d6ebfb062578d
Lubos Vozdecky <lubos.vozdecky@cern.ch>
2020-04-29 07:56:00 +0100

    create /output folder

--- ThermalImpedanceQA/data/.#test.sh
+++
@@ -1,1 +0,0 @@
-vozdecky@heppc402.ph.qmul.ac.uk.757386:1581528688
--- ThermalImpedanceQA/runAll.sh
+++ ThermalImpedanceQA/runAll.sh
@@ -2,6 +2,12 @@
 
 # This script runs the python script in impedancesFromCSV.py on every .csv file in the /data folder
 
+if [ ! -d ./output ]
+    then
+    echo "The output folder doesn't exist. Creating /output."
+    mkdir output
+fi
+
 for file in `ls data/*.csv`; do
     ./impedanceFromCSV.py $file
 done
